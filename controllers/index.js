61c28ef8f6d39a69a1846c2ff606e6adf02c567f
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2023-01-08 14:58:46 -0500

    Tutor session:initial setup of handlebars and helper explanation

--- controllers/index.js
+++ controllers/index.js
@@ -3,4 +3,21 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+router.get('/', (req, res) => {
+    res.render('homepage', {title: 'Homepage'})
+})
+
+router.get('/login', (req,res) => {
+    res.render('login', {title: 'Login'})
+})
+
+router.get('/dashboard', (req,res) => {
+    res.render('dashboard', {title: 'Dashboard'})
+})
+
+router.get('/post/:id', (req,res) => {
+    res.render('post', {title: 'Post', postId: req.params.id})
+})
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const handlebars = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', handlebars())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 
 const sess = {
   secret: 'Super secret secret',
