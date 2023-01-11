8d5bd932f0c5887c6184bf7ace0fa1fb9cae0c65
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2023-01-11 14:53:42 -0500

    Copied over code from miniactivity to adjust for my prohjects need

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// Check if logged in. If not, redirect to login page. Otherwise, continue to designated page. 
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+  
